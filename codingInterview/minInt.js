1fae1fb2c2312cc5bada5e4410f54b985466ab55
gizem <gizemtuncer17@gmail.com>
2023-02-20 15:11:27 +0300

    edited minInt.js

--- codingInterview/minInt.js
+++ codingInterview/minInt.js
@@ -1,22 +1,29 @@
-/*How we can find that the smallest integer of array list */
+ /*How we can find that the smallest integer of array list */ 
 
-function minInt(arr) {
-  let min = 0;
-  let isMin;
-  for (let i of arr) {
-    if (i > 0) {
-      min = i;
-      min > isMin ? (isMin = isMin) : (isMin = min);
-    }
-    if (i === 0) {
-      min = 0;
-      isMin = 0;
-    } else {
-      isMin = 0;
-    }
-  }
-  console.log({ result: isMin });
+
+function minInt(arr){
+    let min = 0
+    let isMin ;
+    for (let i of arr) {
+        if (i >= 0){
+            min = i;
+            min > isMin ? isMin=isMin : isMin=min
+        }
+        else{
+            isMin = 0;
+        }
 }
 
-arr1 = [0, 2, 3, 89, -87];
-minInt(arr1);
+console.log({result :isMin});
+}
+
+arr1 = [2,3,89,0];
+minInt(arr1)
+
+//*Other ways 
+// const arr = [3, 4, 20];
+// function positiveMinimum(array) {
+//     let arraySorted = array.sort((a,b) => a - b);
+//     if(arraySorted[0] > 0){return arraySorted[0]} else{return 0}
+// }
+// console.log(positiveMinimum(arr))
